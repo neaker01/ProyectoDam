b03f3eefc20ec8eefe4f72c5a6646ed732cf5f4d
Neaker <snakeone93@hotmail.com>
2019-09-11 22:12:41 +0200

    cambios

--- app/src/main/java/com/example/proyectodam/Inicial.java
+++ app/src/main/java/com/example/proyectodam/Inicial.java
@@ -1,13 +1,76 @@
 package com.example.proyectodam;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 public class Inicial extends AppCompatActivity {
+
+    private Button btPerfil;
+    private Button btTests;
+    private Button btVideos;
+    private Button btcontacto;
+    private Button btLogout;
+    private Preferencias preferencias;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial);
+        init();
+
+
+
+        btPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                preferencias.eliminarPreferencias();
+                Intent i = new Intent(Inicial.this, Perfil.class);
+                startActivity(i);
+            }
+        });
+
+
+        btTests.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                preferencias.eliminarPreferencias();
+                Intent i = new Intent(Inicial.this, Tests.class);
+                startActivity(i);
+            }
+        });
+
+        btLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                preferencias.eliminarPreferencias();
+                Intent i = new Intent(Inicial.this, Login.class);
+                startActivity(i);
+            }
+        });
+
+
+
+    }
+
+
+
+
+    private void init() {
+        btPerfil=findViewById(R.id.btPerfil);
+        btTests=findViewById(R.id.btLista);
+        btVideos=findViewById(R.id.btVideos);
+        btcontacto=findViewById(R.id.btContacto);
+        btLogout=findViewById(R.id.btLogout);
+        preferencias = new Preferencias(getApplicationContext());
     }
 }
