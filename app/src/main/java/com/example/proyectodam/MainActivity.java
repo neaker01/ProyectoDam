77178c00cb9e823e43ca93d36a0fe5bfe02eb28e
Neaker <snakeone93@hotmail.com>
2019-09-11 19:42:25 +0200

    prueba

---
+++ app/src/main/java/com/example/proyectodam/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.proyectodam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("El cambio es este");
+    }
+}
