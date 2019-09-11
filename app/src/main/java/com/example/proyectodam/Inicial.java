b49c8221ad9362fe12a358ccb37b0dc054878654
Neaker <snakeone93@hotmail.com>
2019-09-11 22:59:59 +0200

    cambios

--- app/src/main/java/com/example/proyectodam/Inicial.java
+++ app/src/main/java/com/example/proyectodam/Inicial.java
@@ -1,16 +1,11 @@
 package com.example.proyectodam;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 
 public class Inicial extends AppCompatActivity {
 
--- app/src/main/java/com/example/proyectodam/Tests.java
+++ app/src/main/java/com/example/proyectodam/Tests.java
@@ -1,6 +1,6 @@
 package com.example.proyectodam;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Tests extends AppCompatActivity {
